210a4af070970f56c9d1ab2c752999a58582832d
Nahar Nafi <nahar.nafi@fulbrightmail.org>
2021-02-12 17:36:44 +0700

    making information part in the landing page,  integrating  react-leaflet-map into information part

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Information from "../../components/Information";
 const LandingPage = () => {
-  return <div></div>;
+	return (
+		<div>
+			<Information />
+		</div>
+	);
 };
 
 export default LandingPage;
