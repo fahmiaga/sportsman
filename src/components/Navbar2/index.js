1e0d8cca6cb5f048fa01c9d7c438bdb0c3f562ac
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-02-18 22:42:38 +0700

    adding different navbar

---
+++ src/components/Navbar2/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "../../assets/img/logo/Logo.png";
+import { useHistory } from "react-router-dom";
+
+const Navbar = () => {
+  const history = useHistory();
+
+  const handleHome = () => {
+    history.push("/");
+  };
+
+  const handleSignIn = () => {
+    history.push("/login");
+  };
+
+  return (
+    <>
+      <nav className="layout2">
+        <div className="layout2__logo">
+          <img
+            src={logo}
+            onClick={handleHome}
+            alt="logo"
+            className="layout2__img"
+          />
+        </div>
+
+        <button onClick={handleSignIn} className="layout2__button">
+          Logout
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
