4bebb415f3030a77ade82b3f211909e520373d41
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-02-18 22:40:05 +0700

    adding route for page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Feature from "./pages/Features";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 
@@ -11,8 +16,12 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={LandingPage} />
+          <Route path="/feature" component={Feature} />
+          <Route path="/dashboard" component={Dashboard} />
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/about" component={About} />
         </Switch>
       </BrowserRouter>
       <Footer />
