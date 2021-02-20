bd96e2117b0d637dded2e7f346d3b915042780f4
Nahar Nafi <nahar.nafi@fulbrightmail.org>
2021-02-20 19:41:38 +0700

    adding about page

--- src/components/JumbotronUp/index.js
+++ src/components/JumbotronUp/index.js
@@ -4,43 +4,48 @@ import Navbar from "../Navbar1";
 import background from "../../assets/img/jumbotron/jumbotron-nav.png";
 
 function JumbotronUp() {
-  return (
-    <div>
-      <Jumbotron
-        className="background"
-        style={{
-          backgroundImage: `url(${background})`,
-          marginBottom: `0`,
-          borderRadius: `0`,
-        }}
-      >
-        <Navbar />
-        <h1 className="background__text">
-          Track every aspect of your workouts.{" "}
-        </h1>
-        <button className="background__button">Ready to Sweat</button>
-      </Jumbotron>
+	return (
+		<div>
+			<Jumbotron
+				className="background"
+				style={{
+					backgroundImage: `url(${background})`,
+					marginBottom: `0`,
+					borderRadius: `0`,
+				}}
+			>
+				<Navbar />
+				<h1 className="background__text">
+					Track every aspect of your workouts.{" "}
+				</h1>
+				<button className="background__button">
+					Ready to Sweat
+				</button>
+			</Jumbotron>
 
-      <div>
-        <Jumbotron
-          style={{
-            backgroundColor: `rgba(54, 54, 54, 1)`,
-            marginBottom: `0`,
-            borderRadius: `0`,
-          }}
-        >
-          <Container className="body__text">
-            <h1>It all starts with the best tracking</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              posuere sit amet erat nec ullamcorper. Nunc luctus maximus erat a
-              imperdiet.
-            </p>
-          </Container>
-        </Jumbotron>
-      </div>
-    </div>
-  );
+			<div>
+				<Jumbotron
+					style={{
+						backgroundColor: `rgba(54, 54, 54, 1)`,
+						marginBottom: `0`,
+						borderRadius: `0`,
+					}}
+				>
+					<Container className="body__text">
+						<h1>
+							It all starts with the best tracking
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet,
+							consectetur adipiscing elit. Vivamus
+							posuere sit amet erat nec ullamcorper.
+							Nunc luctus maximus erat a imperdiet.
+						</p>
+					</Container>
+				</Jumbotron>
+			</div>
+		</div>
+	);
 }
 
 export default JumbotronUp;
