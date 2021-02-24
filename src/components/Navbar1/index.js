d6532f833640defe409b3806f8d0fce0ea0bea7c
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-02-24 10:59:23 +0700

    adding new navbar

--- src/components/Navbar1/index.js
+++ src/components/Navbar1/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../../assets/img/logo/orange.png';
 import { useHistory } from 'react-router-dom';
+import Headroom from 'react-headroom';
+import _ from 'lodash';
+import jwt_decode from 'jwt-decode';
 
 const Navbar = () => {
 	const [scrollY, setScrollY] = useState(0);
@@ -13,6 +16,7 @@ const Navbar = () => {
 	};
 
 	const history = useHistory();
+
 	const page = window.location.pathname.substring(1);
 	console.log(page);
 
@@ -32,39 +36,125 @@ const Navbar = () => {
 		history.push('/about');
 	};
 
+	const handleContactUs = () => {
+		history.push('/contactus');
+	};
+
+	const handleProfile = () => {
+		history.push('/profile');
+		window.location.reload(true);
+	};
+
 	const handleSignOut = () => {
 		localStorage.removeItem('token');
 		history.push('/');
+		window.location.reload(true);
 	};
 	const token = localStorage.getItem('token');
 
-	return (
-		<>
-			<nav className={`layout ${scrollY > 230 ? 'layout--orange' : ''}`}>
-				<div className='layout__logo'>
-					<img src={logo} onClick={handleHome} alt='logo' className='layout__img' />
+	let decoded;
+	if (token && !_.isEmpty(token)) decoded = jwt_decode(token);
 
-					<input type='checkbox' className='menu-btn' id='menu-btn' />
-					<label htmlFor='menu-btn' className='menu-icon'>
-						<span className='menu-icon__line'></span>
-					</label>
-					<ul className='layout__list'>
-						<li onClick={handleFeature}>Feature</li>
-						<li onClick={handleAbout}>About</li>
-					</ul>
-				</div>
-				{token ? (
-					<button onClick={handleSignOut} className='layout__button'>
-						Sign Out
-					</button>
-				) : (
-					<button onClick={handleSignIn} className='layout__button'>
-						Ready To Sweat?
-					</button>
-				)}
-			</nav>
-		</>
+	return (
+		<Headroom>
+			<header className={`header ${scrollY > 230 ? 'layout--orange' : ''}`}>
+				<img src={logo} onClick={handleHome} alt='' className='layout__img' />
+				<input className='menu-btn' type='checkbox' id='menu-btn' />
+				<label className='menu-icon' htmlFor='menu-btn' style={{ color: 'white' }}>
+					<span className='navicon'></span>
+				</label>
+				<ul className='menu'>
+					<li onClick={handleFeature}>Feature</li>
+					<li onClick={handleAbout}>About</li>
+					<li onClick={handleContactUs}>Contact Us</li>
+					{token ? (
+						<div>
+							<li onClick={handleProfile} class='menu-item'>
+								Welcome {decoded.name}!
+							</li>
+							<li onClick={handleSignOut} className='border-li'>
+								Sign Out
+							</li>
+						</div>
+					) : (
+						// <div>
+						// 	<p className='layout__button'>Welcome {decoded.name}!</p>
+						// </div>
+						<button onClick={handleSignIn} className='layout__button'>
+							Login
+						</button>
+					)}
+				</ul>
+			</header>
+		</Headroom>
 	);
 };
 
 export default Navbar;
+
+// const Navbar = () => {
+// 	const [scrollY, setScrollY] = useState(0);
+// 	useEffect(() => {
+// 		window.addEventListener('scroll', handleScroll);
+// 	});
+
+// 	const handleScroll = () => {
+// 		setScrollY(window.scrollY);
+// 	};
+
+// 	const history = useHistory();
+// 	const page = window.location.pathname.substring(1);
+// 	console.log(page);
+
+// 	const handleHome = () => {
+// 		history.push('/');
+// 	};
+
+// 	const handleSignIn = () => {
+// 		history.push('/login');
+// 	};
+
+// 	const handleFeature = () => {
+// 		history.push('/feature');
+// 	};
+
+// 	const handleAbout = () => {
+// 		history.push('/about');
+// 	};
+
+// 	const handleSignOut = () => {
+// 		localStorage.removeItem('token');
+// 		history.push('/');
+// 	};
+// 	const token = localStorage.getItem('token');
+
+// 	return (
+// 		<>
+// 			<nav className={`layout ${scrollY > 230 ? 'layout--orange' : ''}`}>
+// 				<div className='layout__logo'>
+// 					<img src={logo} onClick={handleHome} alt='logo' className='layout__img' />
+
+// 					<input type='checkbox' className='menu-btn' id='menu-btn' />
+// 					<label htmlFor='menu-btn' className='menu-icon'>
+// 						<span className='menu-icon__line'></span>
+// 					</label>
+// 					<ul className='layout__list'>
+// 						<li onClick={handleFeature}>Feature</li>
+// 						<li onClick={handleAbout}>About</li>
+// 					</ul>
+// 				</div>
+// 				{token ? (
+// 					<button onClick={handleSignOut} className='layout__button'>
+// 						Sign Out
+// 					</button>
+// 				) : (
+// 					<button onClick={handleSignIn} className='layout__button'>
+// 						Ready To Sweat?
+// 					</button>
+// 				)}
+// 			</nav>
+// 		</>
+// 	);
+// };
+
+// export default Navbar;
