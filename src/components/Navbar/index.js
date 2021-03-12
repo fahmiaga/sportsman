fddf7899593bd8975d0d2508cc720bf16c5609b9
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-12 11:07:09 +0000

    Feature/profiles

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,49 +1,190 @@
-import React, { useState, useEffect } from "react";
-import logo from "../../assets/img/logo/Logo.png";
-import {useHistory} from 'react-router-dom';
+// import React, { useState, useEffect } from "react";
+// import Headroom from "react-headroom";
+// import logo from "../../assets/img/logo/orange.png";
+// import img from "../../assets/img/anonymous.jpg";
+// import { useSelector, useDispatch } from "react-redux";
+// import { useHistory } from "react-router-dom";
+// import { getUserData } from "../../redux/Action/userAction";
+// import {
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   NavbarBrand,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   ButtonDropdown,
+//   DropdownToggle,
+//   DropdownMenu,
+//   DropdownItem,
+// } from "reactstrap";
 
-function Navbar() {
-  const [scrollY, setScrollY] = useState(0);
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-  });
+// const Navbars = () => {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const handleScroll = () => {
-    setScrollY(window.setScrollY);
-  };
+//   const toggles = () => setIsOpen(!isOpen);
 
-  const history = useHistory();
+//   useEffect(() => {
+//     window.addEventListener("scroll", handleScroll);
+//   });
 
-  const handleSignIn = () => {
-    history.push('/login')
-  };
+//   const userProfile = useSelector((state) => state.users.userProfile);
+//   const dispatch = useDispatch();
 
-  return (
-    <>
-    <nav className={`layout ${scrollY > 20 ? "layout--white" : ""}`}>
-      <div className="layout__logo">
-        <a href="/">
-          <img src={logo} alt="logo" className="layout__img" />
-        </a>
+//   const [scrollY, setScrollY] = useState(0);
+//   const handleScroll = () => {
+//     setScrollY(window.scrollY);
+//   };
 
-        <input type="checkbox" className="menu-btn" id="menu-btn" />
-        <label htmlFor="menu-btn" className="menu-icon">
-          <span className="menu-icon__line"></span>
-        </label>
-        <ul className="layout__list">
-          <li>
-            <a href="feature">Feature</a>
-          </li>
-          <li>
-            <a href="about">About</a>
-          </li>
-        </ul>
-      </div>
+//   const [dropdownOpen, setOpen] = useState(false);
 
-      <button onClick={handleSignIn} className="layout__button">Ready To Sweat?</button>
-    </nav>
-    </>
-  );
-}
+//   const toggle = () => setOpen(!dropdownOpen);
+//   const history = useHistory();
+//   const image = useSelector((state) => state.users.uploadImg);
 
-export default Navbar;
+//   const page = window.location.pathname.substring(1);
+//   console.log(page);
+
+//   const handleHome = () => {
+//     history.push("/");
+//   };
+
+//   const handleSignIn = () => {
+//     history.push("/login");
+//   };
+
+//   const handleFeature = () => {
+//     history.push("/feature");
+//   };
+
+//   const handleAbout = () => {
+//     history.push("/about");
+//   };
+
+//   const handleContactUs = () => {
+//     history.push("/contactus");
+//   };
+
+//   const handleProfile = () => {
+//     history.push("/profile");
+//     window.location.reload(true);
+//   };
+
+//   const handleHistory = () => {
+//     history.push("/history");
+//     window.location.reload(true);
+//   };
+
+//   const handleFavorite = () => {
+//     history.push("/bookmark");
+//     window.location.reload(true);
+//   };
+
+//   const handleSignOut = () => {
+//     localStorage.removeItem("token");
+//     history.push("/");
+//     window.location.reload(true);
+//   };
+
+//   useEffect((res) => {
+//     dispatch(getUserData());
+//     console.log("ini userProfile", userProfile);
+//     if (userProfile && userProfile.status === 200) {
+//       localStorage.setItem("token", res.data.data);
+//     }
+//   }, []);
+
+//   return (
+//     <Headroom>
+//       <header className={`header ${scrollY > 230 ? "layout--orange" : ""}`}>
+//         <Navbar dark expand="md">
+//           <NavbarBrand>
+//             <img
+//               src={logo}
+//               onClick={handleHome}
+//               alt=""
+//               className="layout__img"
+//             />
+//           </NavbarBrand>
+//           <NavbarToggler onClick={toggles} />
+//           <Collapse
+//             isOpen={isOpen}
+//             navbar
+//             style={{ justifyContent: "flex-end" }}
+//           >
+//             <Nav className="me-auto" navbar>
+//               <NavItem>
+//                 <NavLink onClick={handleFeature}>
+//                   <span className="nav-span">Feature</span>
+//                 </NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink onClick={handleAbout}>
+//                   <span className="nav-span">About</span>
+//                 </NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink onClick={handleContactUs}>
+//                   <span className="nav-span">Contact Us</span>
+//                 </NavLink>
+//               </NavItem>
+//             </Nav>
+//             <NavItem>
+//               {userProfile ? (
+//                 <div>
+//                   <ButtonDropdown
+//                     isOpen={dropdownOpen}
+//                     toggle={toggle}
+//                     style={{ marginTop: "5px" }}
+//                   >
+//                     <DropdownToggle
+//                       caret
+//                       style={{
+//                         fontWeight: "500",
+//                         backgroundColor: "RGBA(255,255,255,0)",
+//                         border: "none",
+//                       }}
+//                     >
+//                       <img
+//                         className="image-navbar"
+//                         src={
+//                           userProfile && userProfile.images
+//                             ? userProfile.images
+//                             : { img }
+//                         }
+//                         alt=""
+//                       />
+//                       {/* <span>{userProfile && userProfile.name}</span> */}
+//                     </DropdownToggle>
+//                     <DropdownMenu right>
+//                       {/* <DropdownItem header>Header</DropdownItem> */}
+//                       <DropdownItem onClick={handleProfile}>
+//                         Profile
+//                       </DropdownItem>
+//                       <DropdownItem onClick={handleHistory}>
+//                         History Workout
+//                       </DropdownItem>
+//                       <DropdownItem onClick={() => history.push("/bookmark")}>
+//                         Favorite Workout
+//                       </DropdownItem>
+//                       <DropdownItem divider />
+//                       <DropdownItem onClick={handleSignOut}>
+//                         Sign Out
+//                       </DropdownItem>
+//                     </DropdownMenu>
+//                   </ButtonDropdown>
+//                 </div>
+//               ) : (
+//                 <button onClick={handleSignIn} className="layout__button">
+//                   Login
+//                 </button>
+//               )}
+//             </NavItem>
+//           </Collapse>
+//         </Navbar>
+//       </header>
+//     </Headroom>
+//   );
+// };
+
+// export default Navbars;
